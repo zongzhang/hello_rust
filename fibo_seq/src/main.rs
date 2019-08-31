6d412519904f1e6e1412f0229a667afbf536eed0
zongzhang <jingchang_007@163.com>
2019-08-31 15:12:47 +0800

    update file

--- fibo_seq/src/main.rs
+++ fibo_seq/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
+    for n in 1..10 {
+        println!("---{}",fib(n))
+    }
+}
+
+
+//F(1)=1，F(2)=1, F(n)=F(n-1)+F(n-2)（n>=3，n∈N*）
+
+
+fn fib(n:i32) -> i32{
+
+    if n==1 {
+        1
+    }
+    else if n == 2{
+        1
+    } else {
+        fib(n-2) + fib(n-1)
+    }
+
+        
+
 }
