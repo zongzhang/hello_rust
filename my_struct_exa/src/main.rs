b2fda50b82c521ddbb5c8b2b94321d1c04b6c42c
zongzhang <jingchang_007@163.com>
2019-09-11 17:32:33 +0800

    update file

--- my_struct_exa/src/main.rs
+++ my_struct_exa/src/main.rs
@@ -1,10 +1,48 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+//方法
+#[allow(dead_code)]
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+}
+
+#[allow(dead_code)]
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    //关联函数 没有self
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+#[allow(unused_variables)]
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
 
-    println!("mian ji->{}", area(width1, height1));
+
+    println!("mian ji {:#?}", rect1.area());
+//    println!("mian ji->{:?}", area(&rect1));
+
+    let rect2 = Rectangle::square(3);
+    println!("hehe {:#?}", rect2);
+
+    let s = String::from("");
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
-}
+//fn area(rectangle: &Rectangle) -> u32 {
+//    rectangle.width * rectangle.height
+//}
