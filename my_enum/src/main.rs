c165fe3ee6830b7dc176001b3c645db7ab4f5274
zongzhang <jingchang_007@163.com>
2019-09-14 17:22:26 +0800

    update file

--- my_enum/src/main.rs
+++ my_enum/src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let four = IpAddr2::V4(String::from("127.0.0.1"));
+
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move {x:i32,y:i32},
+
+}
+
+impl Message {
+    fn call(&self){
+
+    }
 }
