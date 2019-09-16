f99d6c89b4ab9c0905ea44ee5edfc4d598f6ed68
zongzhang <jingchang_007@163.com>
2019-09-16 19:59:03 +0800

    update file

--- my_error/src/main.rs
+++ my_error/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::ErrorKind
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+//    panic!("crash and burn");
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+    let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+
 }
