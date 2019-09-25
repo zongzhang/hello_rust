b3be14a92f2240f9de9515756c8e8cda3895f13b
zongzhang <jingchang_007@163.com>
2019-09-25 20:51:55 +0800

    update file

---
+++ my_crate/src/main.rs
@@ -0,0 +1,12 @@
+/// 将给定的数字加一
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, my_crate::add_one(5));
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ my_iter/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    //迭代器是惰性的 v1_iter是不可变的 iter_mut() 返回可变
+    let v1_iter = v1.iter();
+
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+    iterator_sum();
+}
+
+//type Item 和 Self::Item，他们定义了 trait 的 关联类型（associated type）
+//trait Iterator {
+//    type Item;
+//
+//    fn next(&mut self) -> Option<Self::Item>;
+//
+// 此处省略了方法的默认实现
+//}
+
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3, ];
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();//sum方法消费适配器
+
+    println!("{}", total);
+}
