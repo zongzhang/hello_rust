e5565dc699e57a0232c454e6c840f7eda546b35e
zongzhang <jingchang_007@163.com>
2019-10-19 19:40:24 +0800

    update file

---
+++ my_oop/src/lib.rs
@@ -0,0 +1,65 @@
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+    where T: Draw {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // 实际绘制按钮的代码
+    }
+}
+
+//如果一个 trait 中所有的方法有如下属性时，则该 trait 是对象安全的：
+//返回值类型不为 Self
+//方法没有任何泛型类型参数
---
+++ my_oop/src/main.rs
@@ -0,0 +1,8 @@
+use my_oop::AveragedCollection;
+
+fn main() {
+    println!("Hello, world!");
+
+//    AveragedCollection::add()
+}
+
