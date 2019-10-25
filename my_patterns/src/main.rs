439affbe147071af5dbb76ca0dab41bd3a53769c
zongzhang <jingchang_007@163.com>
2019-10-25 18:49:45 +0800

    update file

--- my_patterns/src/main.rs
+++ my_patterns/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        _ => println!("ss"),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("got 50"),
+        Some(y) => println!("matched ,y={:?}", y),
+        _ => println!("Default case,x = {:?}", x),
+    };
+
+    println!("at the end : x={:?},y={:?}", x, y);
 }
