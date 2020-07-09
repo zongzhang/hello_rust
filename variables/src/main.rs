f347c544ff713d8eba3fb9c16bc570a6be2fff43
zongzhang <jingchang_007@163.com>
2020-07-09 15:40:36 +0800

    修改变量例子

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,65 +9,26 @@ fn main() {
     y = 6;
     println!("y->{}", y);
 
-    //常量
-    const MAX_POINTS: u32 = 100_000;
-
-    println!("const - > {}", MAX_POINTS);
-
-    //隐藏
-    let z = 5;
-    let z = z + 1;
-    let z = z * 2;
-    println!("z -> {}", z);
+    const Z: u32 = 5;
+    println!("z->{}", Z);
 
     let spaces = "   ";
-    //隐藏了spaces,从新赋值，包括类型也改变了
     let spaces = spaces.len();
 
-    println!("spaces -> {}", spaces);
+    println!("spaces len -> {}", spaces);
 
-    // 类型变了
-    // let mut spaces = "   ";
-    // spaces = spaces.len();
+    let _c = 'z';
 
-    //整形
-    let _z1: i8 = 1;
-    let _z2: u8 = 2;
-    let _z3: i16 = 3;
-    let _z4: u16 = 4;
-    let _z5: i32 = 5;
-    let _z6: u32 = 6;
-    //在64位构架是64，在32位构架是32
-    let _z7: isize = 7;
-    let _z8: usize = 8;
-    //浮点型
-    let _f1: f32 = 1.0;
-    let _f2: f64 = 2.0;
 
-    let sum = 5 + 10;
-    println!("sum->{}", sum);
+    let xx: (i32, f64, u8) = (500, -1.1, 1);
+    let first = xx.0;
+    println!("first->{}", first);
 
-    let bb: bool = true;
-    println!("bb - > {}", bb);
+    let a = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    println!("a-->{}", a[1]);
 
-    //char 是单引号
-    let s = '😻';
-    println!("z->>{}", s);
+    let _b: [i32; 5] = [1, 2, 3, 4, 5];
+    let _c = [3, 5];
 
-    //复合类型 元组 数组
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-
-    //模式匹配来解构
-    let (_x3, _y3, _z3) = &tup;
-
-    println!("_x3->{}", _x3);
-
-    let big_num = tup.0;
-    println!("big_num->{}", big_num);
-
-    let _a = [1, 2, 3, 4, 5, 6];
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    //无效数组
-    println!("ss->>{}", a[10]);
-    println!("arr->{}", a[0]);
 }
