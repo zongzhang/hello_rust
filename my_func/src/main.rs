3ba8a4b47503a5f7fd7ca7066a1ec154fd21587a
zongzhang <jingchang_007@163.com>
2020-07-09 16:37:19 +0800

    修改函数例子

--- my_func/src/main.rs
+++ my_func/src/main.rs
@@ -1,57 +1,31 @@
 fn main() {
     println!("Hello, world!");
     another_function();
+    another_function_with_param(5, 6);
 
-    //这是一个语句，
-    let _y = 6;
-    //语句没有返回值
-    // let x = (let y = 6); 错的
-
-    // {.....} 是表达式
+    // 表达式
     let y = {
         let x = 3;
-        x + 1 // 没有分号
+        x + 1
     };
-    println!("y->{}", y);
 
-    let x = five();
-    println!("x->{}", x);
-
-    let number = 3;
-    // 判断条件返回类型应该是bool
-    if number < 5 {
-        println!("number < 5");
-    } else {
-        println!("number > 5");
-    }
-
-    let condition = true;
-    // if 是个表达式
-    let number = if condition { 55 } else { 66 };
-
-    println!("number -> {}", number);
-
-    let mut counter = 0;
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-    println!("result->{}", result);
-    assert_eq!(result, 20);
-
-    //for 循环
-    let a = [10, 20, 30, 40, 50];
-    for element in a.iter() {
-        println!("the value->{}", element);
-    }
+    println!("five()->{}", five());
+    println!("five_with_param()->{}", five_with_param(5));
 }
 
 fn another_function() {
     println!("another function")
 }
 
+fn another_function_with_param(x: i32, y: i32) {
+    println!("another function x->{},y-{}", x, y);
+}
+
+
 fn five() -> i32 {
     5
+}
+
+fn five_with_param(x: i32) -> i32 {
+    x + 5
 }
