65e841e0eb935abbb7ef5ac01bde3cde166ab3fa
zongzhang <jingchang_007@163.com>
2020-07-10 17:20:21 +0800

    修改结构体和方法、函数例子

--- my_struct_exa/src/main.rs
+++ my_struct_exa/src/main.rs
@@ -1,48 +1,81 @@
-#[derive(Debug)]
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+//
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// 元组重构 元组帮助我们增加了一些结构性
+// fn main() {
+//     let rect1 = (30, 50);
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+//
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+//使用结构体
+#[derive(Debug)] // 调试使用 增加注解来派生 Debug trait，并使用调试格式打印 Rectangle 实例
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
 }
 
-//方法
-#[allow(dead_code)]
+// 结合不够紧密
+// fn main() {
+//     let rect1 = Rectangle { width: 30, height: 50 };
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rect1)
+//     );
+//
+//     println!("rect1 is {:#?}", rect1);
+// }
+//
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// 改写成一个定义于 Rectangle 结构体上的 area 方法
 impl Rectangle {
     fn area(&self) -> u32 {
-        self.height * self.width
+        self.width * self.height
     }
-}
 
-#[allow(dead_code)]
-impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+}
 
-    //关联函数 没有self
+// 多个impl块
+impl Rectangle {
+    // 关联函数，没有self。是函数，不是方法。与结构体实例无关
     fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size
-        }
+        Rectangle { width: size, height: size }
     }
 }
-#[allow(unused_variables)]
+
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50
-    };
+    let rect1 = Rectangle { width: 30, height: 50 };
 
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 
-    println!("mian ji {:#?}", rect1.area());
-//    println!("mian ji->{:?}", area(&rect1));
-
+    //关联函数
     let rect2 = Rectangle::square(3);
-    println!("hehe {:#?}", rect2);
-
-    let s = String::from("");
 }
-
-//fn area(rectangle: &Rectangle) -> u32 {
-//    rectangle.width * rectangle.height
-//}
