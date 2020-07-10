8a6b39667d9a6d15d0abd475217e582fae76afce
zongzhang <jingchang_007@163.com>
2020-07-10 15:53:40 +0800

    添加注释

--- my_struct/src/main.rs
+++ my_struct/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     user1.email = String::from("another@123.com");
     println!("email->{}", user1.email);
 
-    //使用结构体更新语法从其他实例创建实例
+    //使用 结构体更新语法 从其他实例创建实例
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
@@ -31,9 +31,13 @@ fn main() {
     let origin = Point(0, 0, 0);
 }
 
+//结构体
+//String 有意选择的
+//使结构体存储被其他对象拥有的数据的引用，不过这么做的话需要用上 生命周期
 #[allow(dead_code)]
 struct User {
-    username: String, //字段
+    //字段
+    username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
@@ -42,19 +46,10 @@ struct User {
 #[allow(dead_code)]
 fn build_user(email: String, username: String) -> User {
     User {
-        email: email,
-        username: username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
-
-//简写
-#[allow(dead_code)]
-fn build_user2(email: String, username: String) -> User {
-    User {
-        email,
-        username,
+        // email: email,
+        // username: username,
+        email,//简写
+        username,//简写
         active: true,
         sign_in_count: 1,
     }
@@ -62,4 +57,5 @@ fn build_user2(email: String, username: String) -> User {
 
 //元组结构体
 struct Color(i32, i32, i32);
+
 struct Point(i32, i32, i32);
