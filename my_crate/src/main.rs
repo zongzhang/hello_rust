8483ba59abf47cb8629905f7972170233928e7e9
zongzhang <jingchang_007@163.com>
2020-07-14 13:47:47 +0800

    修改注释

--- my_crate/src/main.rs
+++ my_crate/src/main.rs
@@ -1,12 +1,8 @@
-/// 将给定的数字加一
-///
-/// # Examples
-///
-/// ```
-/// let five = 5;
-///
-/// assert_eq!(6, my_crate::add_one(5));
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
-}
+//crate 是一个二进制项或者库
+//一个包中至多 只能 包含一个库 crate(library crate)
+//包中可以包含任意多个二进制 crate(binary crate)
+//包中至少包含一个 crate，无论是库的还是二进制的。
+//约定：src/main.rs 就是一个与包同名的二进制 crate 的 crate 根
+//Cargo 知道如果包目录中包含 src/lib.rs，则包带有与其同名的库 crate，且 src/lib.rs 是 crate 根
+//如果一个包同时含有 src/main.rs 和 src/lib.rs，则它有两个 crate：一个库和一个二进制项，且名字都与包相同
+fn main() {}
