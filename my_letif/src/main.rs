fe7565445bd913140d38ed6d51bc75c6b3fe8316
zongzhang <jingchang_007@163.com>
2020-07-14 11:05:06 +0800

    修改if let语句

--- my_letif/src/main.rs
+++ my_letif/src/main.rs
@@ -1,19 +1,15 @@
 fn main() {
+    // let some_u8_value = Some(3);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
     let some_u8_value = Some(3);
-
     if let Some(3) = some_u8_value {
         println!("three");
-    } else {
-        println!("other");
     }
 }
 
 
-fn con() {
-    let some_u8_value = Some(0u8);
-    match some_u8_value {
-        Some(3) => println!("three"),
-        _ => (),
-    }
-}
+
 
