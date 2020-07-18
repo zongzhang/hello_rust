8aa978b8d781c3f9c24c0431817aef70d092c577
zongzhang <jingchang_007@163.com>
2020-07-18 14:13:03 +0800

    修改panic例子

--- my_error/src/main.rs
+++ my_error/src/main.rs
@@ -1,23 +1,82 @@
 use std::fs::File;
-use std::io::ErrorKind
+use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
+use std::fs;
+
+
+//
 #[allow(unused_variables)]
 fn main() {
-//    panic!("crash and burn");
-
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-            },
-            other_error => panic!("There was a problem opening the file: {:?}", other_error),
-        },
-    };
-    let f = File::open("hello.txt").unwrap();
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // panic! 与不可恢复的错误
+    //release 下终止展开
+    // [profile.release]
+    // panic = 'abort'
+    // panic!("crash and burn");
 
 
+    // let v = vec![1, 2, 3];
+    // v[99]; //panic
+
+    // hello.txt 不存在抛出panic
+    // let f = File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => {
+    //         panic!("Problem opening the file: {:?}", error)
+    //     }
+    // };
+
+    //匹配不同的错误
+    // let f = File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => panic!("Problem opening the file: {:?}", other_error),
+    //     },
+    // };
+
+    //失败时 panic 的简写：unwrap 和 expect
+    // 如果 Result 值是成员 Ok，unwrap 会返回 Ok 中的值。如果 Result 是成员 Err，unwrap 会为我们调用 panic!
+    // let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+
+}
+
+// 传播错误
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let f = File::open("hello.txt");
+//
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+//
+//     let mut s = String::new();
+//
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// 传播错误的简写：? 运算符
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let mut f = File::open("hello.txt")?;
+    // let mut s = String::new();
+    // f.read_to_string(&mut s)?;
+    // Ok(s)
+
+    // 还可以再缩短
+    // let mut s = String::new();
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+    // Ok(s)
+
+    //更简单的
+    fs::read_to_string("hello.txt")
 }
