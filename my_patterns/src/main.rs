504ce3f10f7eda09ba6eb25ec4999e4276c9f0bb
zongzhang <jingchang_007@163.com>
2020-07-19 23:54:04 +0800

    修改范型例子

--- my_patterns/src/main.rs
+++ my_patterns/src/main.rs
@@ -1,18 +1,35 @@
-fn main() {
-    let x = 1;
-    match x {
-        1 => println!("one"),
-        _ => println!("ss"),
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    let x = Some(5);
-    let y = 10;
+    largest
+}
 
-    match x {
-        Some(50) => println!("got 50"),
-        Some(y) => println!("matched ,y={:?}", y),
-        _ => println!("Default case,x = {:?}", x),
-    };
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
 
-    println!("at the end : x={:?},y={:?}", x, y);
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
