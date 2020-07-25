604fd23cfd7b9fb9bdafd51be61c08683aac9363
zongzhang <jingchang_007@163.com>
2020-07-25 11:03:52 +0800

    修改练习项目例子

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,22 +1,29 @@
 use std::env;
 use std::process;
-
 use minigrep;
 use minigrep::Config;
 
 fn main() {
+    // 递给 minigrep 的命令行参数并将其收集到一个 vector 中
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("problem parsing argsuments: {}", err);
-        process::exit(1);
-    });
+    let query = &args[1];
+    let filename = &args[2];
 
-
-    //因为 run 在成功时返回 ()，而我们只关心检测错误，所以并不需要 unwrap_or_else 来返回未封装的值，因为它只会是 ()。
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Appliaction error: {}", e);
-        process::exit(1);
-    }
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    //
+    // let config = Config::new(&args).unwrap_or_else(|err| {
+    //     eprintln!("problem parsing argsuments: {}", err);
+    //     process::exit(1);
+    // });
+    //
+    //
+    // //因为 run 在成功时返回 ()，而我们只关心检测错误，所以并不需要 unwrap_or_else 来返回未封装的值，因为它只会是 ()。
+    // if let Err(e) = minigrep::run(config) {
+    //     eprintln!("Appliaction error: {}", e);
+    //     process::exit(1);
+    // }
 }
 
