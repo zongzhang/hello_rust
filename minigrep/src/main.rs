e6c465fa782ed8938b369ed43b6b7211edc35339
zongzhang <jingchang_007@163.com>
2020-07-28 22:14:33 +0800

    修改练习项目例子

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
-use minigrep;
-use minigrep::Config;
 
 fn main() {
     // 递给 minigrep 的命令行参数并将其收集到一个 vector 中
@@ -12,21 +11,34 @@ fn main() {
     // let query = &args[1];
     // let filename = &args[2];
 
-    let (query, filename) = parse_config(&args);
+    // let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    // println!("Searching for {}", query);
+    // println!("In file {}", filename);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
 
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
 
-
+    Ok(())
 }
-
 // 废弃
 // fn parse_config(args: &[String]) -> (&str, &str) {
 //     let query = &args[1];
@@ -45,4 +57,17 @@ fn parse_config(args: &[String]) -> Config {
     let filename = args[2].clone();
 
     Config { query, filename }
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
