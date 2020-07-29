1837aaf4bbcb26e6cf34d67077ad91851b360ea6
zongzhang <jingchang_007@163.com>
2020-07-29 22:24:06 +0800

    修改项目例子

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     // 递给 minigrep 的命令行参数并将其收集到一个 vector 中
@@ -24,7 +24,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
@@ -32,13 +32,7 @@ fn main() {
 }
 
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
 // 废弃
 // fn parse_config(args: &[String]) -> (&str, &str) {
 //     let query = &args[1];
@@ -47,27 +41,3 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 //     (query, filename)
 // }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
