e0d2a84cca0f4e4efab12caf6a65cd755ffebf64
zongzhang <jingchang_007@163.com>
2020-08-03 17:27:01 +0800

    修改闭包例子

--- my_closures/src/main.rs
+++ my_closures/src/main.rs
@@ -9,51 +9,28 @@ fn main() {
         simulated_user_specified_value,
         simulated_random_number,
     );
-
-
-    let x = 4;
-    // x 在同一个作用域
-    let equal_to_x = |z| z == x;
-
-    let y = 4;
-
-//    assert!(equal_to_x(y));
-//    FnOnce 消费从周围作用域捕获的变量，闭包周围的作用域被称为其 环境，environment。为了消费捕获到的变量，
-//   闭包必须获取其所有权并在定义闭包时将其移动进闭包。其名称的 Once 部分代表了闭包不能多次获取相同变量的所有权的事实，
-//   所以它只能被调用一次。
-//    FnMut 获取可变的借用值所以可以改变其环境
-//    Fn 从其环境获取不可变的借用值
 }
 
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     println!("calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
-//尝试调用一个被推断为两个不同类型的闭包 会出错
-//let example_closure = |x| x;
-//
-//let s = example_closure(String::from("hello"));
-//let n = example_closure(5);
-
-
+// simulated_expensive_calculation 转换为闭包  expensive_closure
 fn generate_workout(intensity: u32, random_number: u32) {
-//    let expensive_result = simulated_expensive_calculation(intensity);
-
-    // |param1, param2|
+    //闭包类型推断和注解
     let expensive_closure = |num| {
-        println!("calulating slowly...");
+        println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     };
-
-//    let expensive_closure = |num: u32| -> u32 {
-//        println!("calculating slowly...");
-//        thread::sleep(Duration::from_secs(2));
-//        num
-//    };
+    // 严格定义的
+    // let expensive_closure = |num: u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
 
     if intensity < 25 {
         println!(
@@ -76,56 +53,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
     }
 }
 
-struct Cacher<T> where T: Fn(u32) -> u32
-{
-    calculation: T,
-    value: Option<u32>,
-}
-
-impl<T> Cacher<T> where T: Fn(u32) -> u32 {
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-fn generate_workout_2(intensity: u32, random_number: u32) {
-    let mut expensive_result = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            expensive_result.value(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            expensive_result.value(intensity)
-        );
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result.value(intensity)
-            );
-        }
-    }
-}
+// let example_closure = |x| x;
+// 第一次使用 String 类型作为参数而第二次使用 u32 错误
+// let s = example_closure(String::from("hello"));
+// let n = example_closure(5);
